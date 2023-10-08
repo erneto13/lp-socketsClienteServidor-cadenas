256c086f97368ac1ed790b892fb290958ca6b8f6
erneto <128459472+erneto13@users.noreply.github.com>
2023-10-08 01:28:48 -0700

    Update socketCliente.cpp

--- lp-socketCliente-Cadena/socketCliente.cpp
+++ lp-socketCliente-Cadena/socketCliente.cpp
@@ -14,7 +14,7 @@ public:
         cout<<"Conectando al servidor..."<<endl<<endl;
         WSAStartup(MAKEWORD(2,0), &WSAData);
         server = socket(AF_INET, SOCK_STREAM, 0);
-        addr.sin_addr.s_addr = inet_addr("192.168.100.10");
+        addr.sin_addr.s_addr = inet_addr("tuIP");
         addr.sin_family = AF_INET;
         addr.sin_port = htons(5555);
         connect(server, (SOCKADDR *)&addr, sizeof(addr));
